3c502645597907954b8bd8ee629deef378354186
antoniorv6 <ariosvila@gmail.com>
2024-05-10 10:27:34 +0200

    Uploaded last version with dependencies fixed

--- visualize_diffs.py
+++ visualize_diffs.py
@@ -4,4 +4,4 @@ from musicdiff import diff, DetailLevel, Visualization
 #Visualization.DELETED_COLOR = "red"
 #Visualization.CHANGED_COLOR = "yellow"
 
-diff("original_m-66-69.krn", "prediction_piano.krn", out_path1="Marked_piano.pdf", out_path2="Marked2.pdf", visualize_diffs=True, detail=DetailLevel.AllObjectsWithStyle)
+diff("gt.krn", "prediction.krn", out_path1="Marked_piano.pdf", out_path2="Marked2.pdf", visualize_diffs=True, detail=DetailLevel.AllObjectsWithStyle)
